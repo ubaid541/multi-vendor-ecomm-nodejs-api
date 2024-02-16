c4f94de16d4bdbbc73ffbce03f474d1560065301
ubaidawan <malikunknown633@gmail.com>
2024-02-16 12:13:34 +0500

    show message for no data in addons. list roles in an object

--- controller/Admin/addonController.js
+++ controller/Admin/addonController.js
@@ -1,89 +1,105 @@
 import { Addon } from "../../models/index.js";
+import { roles } from "../../utils/roles.js";
 
 const addonContoller = {
-    async getAddons(req,res,next){
-        let addons
-        try {
-            if(req.query.user_role === "admin"){
-                 addons = await Addon.find(null,null,{sort : {'createdAt' : -1}}).populate('seller_id')
-            }
-            else if(req.query.user_role === "seller"){
-                 addons = await Addon.find({seller_id:req.query.user_id},null,{sort : {'createdAt' : -1}})
-            }
-            res.status(200).json(addons)
-        } catch (error) {
-            next(error)
-        }
+  async getAddons(req, res, next) {
+    let addons;
+    try {
+      if (!req.query.user_role || !roles[req.query.user_role]) {
+        return res
+          .status(403)
+          .json({ message: "Invalid user role.", error: 403 });
+      }
+      if (req.query.user_role === "admin") {
+        addons = await Addon.find(null, null, {
+          sort: { createdAt: -1 },
+        }).populate("seller_id");
 
-    },
-    async getSingleAddon(req,res,next){
+        console.log("addons list:: ", addons);
+      } else if (req.query.user_role === "seller") {
+        addons = await Addon.find({ seller_id: req.query.user_id }, null, {
+          sort: { createdAt: -1 },
+        });
+        console.log("addons list:: ", addons);
+      }
 
-        try {
-                 const single_addon = await Addon.findOne({_id:req.params.id})
-                
-            res.status(200).json(single_addon)
-        } catch (error) {
-            next(error)
-        }
-    },
-    async addAddon(req,res,next){
-        try {
-            const exists = await Addon.exists({addon_name:req.body.addon_name})
+      if (addons.length == 0) {
+        res.status(200).json({ addons, message: "No addons found." });
+        return;
+      }
+      res.status(200).json(addons);
+    } catch (error) {
+      next(error);
+    }
+  },
+  async getSingleAddon(req, res, next) {
+    try {
+      const single_addon = await Addon.findOne({ _id: req.params.id });
 
-            if(exists){
-                return res.status(409).send("Addon name already exists.")
-            }
+      res.status(200).json(single_addon);
+    } catch (error) {
+      next(error);
+    }
+  },
+  async addAddon(req, res, next) {
+    try {
+      const exists = await Addon.exists({ addon_name: req.body.addon_name });
 
-        } catch (error) {
-            next(error)
-        }
-
-        const {addon_name,addon_price} = req.body
-
-        const newAddon = new Addon({
-            ...req.body,
-            seller_id : req.query.user_id
-        })
-
-
-        try {
-           await newAddon.save()
-           res.status(200).json(["New Addon Added",newAddon])
-        } catch (error) {
-            next(error)
-        }
-    },
-    async deleteAddon(req,res,next){
-        try {
-            const deleteAddon = await Addon.deleteOne({_id:req.params.id})
-            res.status(200).json("Addon Deleted.")
-        } catch (error) {
-            next(error)
-        }
-    },
-    async updateAddon(req,res,next){
-
-        try {
-            const exists = await Addon.exists({$and : [{addon_name:req.body.addon_name},{_id : {$ne : req.query.addon_id}}]})
-
-            if(exists){
-                return res.status(409).send("Addon name already exists.")
-            }
-
-        } catch (error) {
-            next(error)
-        }
-
-        try {
-            const updateAddon = await Addon.updateOne({_id:req.query.addon_id},{$set:req.body})
-            res.status(200).json("Addon Updated.")
-
-        } catch (error) {
-            next(error)
-        }
+      if (exists) {
+        return res.status(409).send("Addon name already exists.");
+      }
+    } catch (error) {
+      next(error);
     }
 
-}
+    const { addon_name, addon_price } = req.body;
 
-export default addonContoller
+    const newAddon = new Addon({
+      ...req.body,
+      seller_id: req.query.user_id,
+    });
 
+    try {
+      await newAddon.save();
+      res.status(200).json(["New Addon Added", newAddon]);
+    } catch (error) {
+      next(error);
+    }
+  },
+  async deleteAddon(req, res, next) {
+    try {
+      const deleteAddon = await Addon.deleteOne({ _id: req.params.id });
+      res.status(200).json("Addon Deleted.");
+    } catch (error) {
+      next(error);
+    }
+  },
+  async updateAddon(req, res, next) {
+    try {
+      const exists = await Addon.exists({
+        $and: [
+          { addon_name: req.body.addon_name },
+          { _id: { $ne: req.query.addon_id } },
+        ],
+      });
+
+      if (exists) {
+        return res.status(409).send("Addon name already exists.");
+      }
+    } catch (error) {
+      next(error);
+    }
+
+    try {
+      const updateAddon = await Addon.updateOne(
+        { _id: req.query.addon_id },
+        { $set: req.body }
+      );
+      res.status(200).json("Addon Updated.");
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default addonContoller;
